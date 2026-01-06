f2e06f8cbba7bc8588a9acf90f0a29b23ae2a18e
ozan-fn <me@ozan.my.id>
2026-01-06 21:55:13 +0700

    refactor: replace Google Analytics script with Clarity tracking script

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -48,14 +48,12 @@
         <meta name="description" content="{{ $meta }}">
     @endif
 
-    {{-- Google tag (gtag.js) --}}
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-4N8JCVN8LN"></script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag() { dataLayer.push(arguments); }
-        gtag('js', new Date());
-
-        gtag('config', 'G-4N8JCVN8LN');
+    <script type="text/javascript">
+        (function (c, l, a, r, i, t, y) {
+            c[a] = c[a] || function () { (c[a].q = c[a].q || []).push(arguments) };
+            t = l.createElement(r); t.async = 1; t.src = "https://www.clarity.ms/tag/" + i;
+            y = l.getElementsByTagName(r)[0]; y.parentNode.insertBefore(t, y);
+        })(window, document, "clarity", "script", "ux9cg8p64m");
     </script>
 </head>
 
